383cc789bb94f04628634f108351057c018be0b9
Edgar Bjørntvedt <edgar.bjorntvedt@gmail.com>
2024-01-27 09:58:46 +0100

    fixed types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,6 @@
 export default class ExtendedError extends Error {
     constructor(message: string, content?: object)
 }
+export class ExtendedError extends Error {
+    constructor(message: string, content?: object)
+}
